6c2b7cc7e1f0e9bb627410556ca2448737aa93cc
mouwade <ahmedwade001@gmail.com>
2025-12-31 02:34:47 +0000

    Remove unused body selector from script for cleaner code

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const body = document.querySelector("body");
 const navBtn = document.querySelector(".nav__btn");
 const nav = document.querySelector(".nav");
 const img = navBtn.querySelector(".nav__icon");
